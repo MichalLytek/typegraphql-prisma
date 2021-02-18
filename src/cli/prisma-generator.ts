95354e771708a3a041b664559dbf6af83647aade
Steve Fan <29133953+stevefan1999-personal@users.noreply.github.com>
2021-02-18 09:23:24 +0000

    add resolver for missing custom scalar fields

--- src/cli/prisma-generator.ts
+++ src/cli/prisma-generator.ts
@@ -3,15 +3,43 @@ import { DMMF as PrismaDMMF } from "@prisma/client/runtime";
 import { promises as asyncFs } from "fs";
 import path from "path";
 import { unflatten } from "flat";
-import { pick } from "underscore"
+import _ from "underscore";
 
 import generateCode from "../generator/generate-code";
 import removeDir from "../utils/removeDir";
-import { GenerateCodeOptions } from "../generator/options";
+import { GenerateCodeOptions, ScalarTypesOptions } from "../generator/options";
 import { toUnixPath } from "../generator/helpers";
 
 function parseStringBoolean(stringBoolean: string | undefined) {
   return stringBoolean ? stringBoolean === "true" : undefined;
+}
+
+function resolveCustomScalar(
+  oldTypes?: Record<string, ScalarTypesOptions>,
+): Record<string, ScalarTypesOptions> {
+  const types = _.chain(oldTypes ?? {})
+    // not both field and graphql entry supplied -- skip
+    .pick((value, _) => !!(value.field && value.graphql))
+    // none of the entries had field and graphql type supplied -- skip
+    .pick((value, _) => !!(value!.field!.type || value!.graphql!.type))
+    // none of the entries had field and graphql module supplied -- skip
+    .pick((value, _) => !!(value!.field!.module || value!.graphql!.module))
+    .value() as object;
+
+  for (const [_, value] of Object.entries(types ?? {})) {
+    // one of the entry has type name -- automatic deduction from the entry with non empty module name
+    if (!!value.field.type !== !!value.graphql.type) {
+      value.graphql.type = value.graphql.type ?? value.field.type;
+      value.field.type = value.field.type ?? value.graphql.type;
+    }
+
+    // ditto
+    if (!!value.field.module !== !!value.graphql.module) {
+      value.graphql.module = value.graphql.module ?? value.field.module;
+      value.field.module = value.field.module ?? value.graphql.module;
+    }
+  }
+  return types as Record<string, ScalarTypesOptions>;
 }
 
 export async function generate(options: GeneratorOptions) {
@@ -26,6 +54,17 @@ export async function generate(options: GeneratorOptions) {
     .dmmf as PrismaDMMF.Document;
 
   const generatorConfig = options.generator.config;
+  const {
+    types = {},
+  }: {
+    types?: Record<string, ScalarTypesOptions>;
+  } = unflatten(
+    _.pick(generatorConfig, (_: string, key: string) =>
+      key.startsWith("types"),
+    ),
+    { delimiter: "_" },
+  );
+
   const config: GenerateCodeOptions = {
     emitDMMF: parseStringBoolean(generatorConfig.emitDMMF),
     emitTranspiledCode: parseStringBoolean(generatorConfig.emitTranspiledCode),
@@ -41,12 +80,8 @@ export async function generate(options: GeneratorOptions) {
     useUncheckedScalarInputs: parseStringBoolean(
       generatorConfig.useUncheckedScalarInputs,
     ),
-    types: unflatten(
-      pick(generatorConfig, (_: string, key: string) => key.startsWith("types")), 
-      { delimiter: '_' }
-    )
+    types: resolveCustomScalar(types),
   };
-  console.log(JSON.stringify(config.types))
 
   if (config.emitDMMF) {
     await Promise.all([
