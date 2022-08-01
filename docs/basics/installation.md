2f885651d0c623353d8b6c05c0ab1bb10baa6e33
Michał Lytek <michal.wojciech.lytek@gmail.com>
2022-08-01 13:57:45 +0200

    Upgrade website Docusarus to stable v2

--- docs/basics/installation.md
+++ docs/basics/installation.md
@@ -18,14 +18,14 @@ https://typegraphql.com/docs/installation.html
 After that, you have to install the generator, as a dev dependency:
 
 ```sh
-npm i -D typegraphql-prisma
+$ npm i -D typegraphql-prisma
 ```
 
 Furthermore, `typegraphql-prisma` requires Prisma to work properly, so please install Prisma dependencies if you don't have it already installed:
 
 ```sh
-npm i -D prisma
-npm i @prisma/client
+$ npm i -D prisma
+$ npm i @prisma/client
 ```
 
 :::caution
@@ -40,19 +40,19 @@ Otherwise, the runtime check will report an error when you run the generator.
 You also need to install the GraphQL Scalars library (to support the Prisma `Json`, `BigInt` and `Byte` types):
 
 ```sh
-npm i graphql-scalars
+$ npm i graphql-scalars
 ```
 
 In order to properly support the aggregate and group by queries, the `graphql-fields` package is used, so it also has to be installed:
 
 ```sh
-npm i graphql-fields @types/graphql-fields
+$ npm i graphql-fields @types/graphql-fields
 ```
 
 Finally, please also install the `tslib` package, which is required for [applying the additional decorators](../advanced/additional-decorators.md) properly:
 
 ```sh
-npm i tslib
+$ npm i tslib
 ```
 
 ### TypeScript configuration
--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,14 +1,19 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
 const vsLightTheme = require("prism-react-renderer/themes/vsLight");
 const vsDarkTheme = require("prism-react-renderer/themes/vsDark");
 
 // edit default full white background
-vsLightTheme.plain.backgroundColor = "rgba(0, 0, 0, 0.05)";
+// @ts-ignore
+vsLightTheme.plain.backgroundColor = "var(--ifm-code-background)";
 // fix red attribute names
+// @ts-ignore
 vsLightTheme.styles.find(it => it.types.includes("attr-name")).style.color =
-  "rgb(0, 0, 0)";
+  "var(--prism-color);";
 
-/** @type {import('@docusaurus/types').DocusaurusConfig} */
-module.exports = {
+/** @type {import('@docusaurus/types').Config} */
+const config = {
   title: "TypeGraphQL Prisma",
   tagline:
     "Prisma generator to emit TypeGraphQL type classes and CRUD resolvers from your Prisma schema",
@@ -17,98 +22,25 @@ module.exports = {
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
-  organizationName: "MichalLytek",
-  projectName: "typegraphql-prisma",
-  themeConfig: {
-    prism: {
-      theme: vsLightTheme,
-      darkTheme: vsDarkTheme,
-    },
-    algolia: {
-      appId: "G4ZB1BCPHE",
-      apiKey: "02a8d497c435602dcd39b67e73d7e53a",
-      indexName: "typegraphql_prisma",
-      // Optional: see doc section below
-      contextualSearch: true,
-      // Optional: Algolia search parameters
-      searchParameters: {
-        // facetFilters: ["type:content"],
-      },
-      // Set debug to true if you want to inspect the modal
-      debug: false,
-    },
-    navbar: {
-      title: "TypeGraphQL Prisma",
-      logo: {
-        alt: "TypeGraphQL Logo",
-        src: "img/logo.png",
-      },
-      items: [
-        {
-          type: "doc",
-          docId: "intro",
-          position: "left",
-          label: "Docs",
-        },
-        {
-          type: "doc",
-          docId: "examples",
-          position: "left",
-          label: "Examples",
-        },
-        {
-          href: "https://github.com/MichalLytek/typegraphql-prisma",
-          label: "GitHub",
-          position: "right",
-        },
-      ],
-    },
-    footer: {
-      style: "dark",
-      links: [
-        {
-          title: "Docs",
-          items: [
-            {
-              label: "Docs",
-              to: "/docs/intro",
-            },
-            {
-              label: "Examples",
-              to: "/docs/examples",
-            },
-          ],
-        },
-        {
-          title: "Community",
-          items: [
-            {
-              label: "Stack Overflow",
-              href: "https://stackoverflow.com/questions/tagged/typegraphql",
-            },
-            {
-              label: "Twitter",
-              href: "https://twitter.com/typegraphql",
-            },
-          ],
-        },
-        {
-          title: "More",
-          items: [
-            {
-              label: "GitHub",
-              href: "https://github.com/MichalLytek/typegraphql-prisma",
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} Michał Lytek`,
-    },
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "MichalLytek", // Usually your GitHub org/user name.
+  projectName: "typegraphql-prisma", // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
   },
+
   presets: [
     [
-      "@docusaurus/preset-classic",
-      {
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
         docs: {
           path: "../docs",
           sidebarPath: require.resolve("./sidebars.js"),
@@ -118,7 +50,98 @@ module.exports = {
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
-      },
+      }),
     ],
   ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "TypeGraphQL Prisma",
+        logo: {
+          alt: "TypeGraphQL Logo",
+          src: "img/logo.png",
+        },
+        items: [
+          {
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "Docs",
+          },
+          {
+            type: "doc",
+            docId: "examples",
+            position: "left",
+            label: "Examples",
+          },
+          {
+            href: "https://github.com/MichalLytek/typegraphql-prisma",
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Docs",
+            items: [
+              {
+                label: "Docs",
+                to: "/docs/intro",
+              },
+              {
+                label: "Examples",
+                to: "/docs/examples",
+              },
+            ],
+          },
+          {
+            title: "Community",
+            items: [
+              {
+                label: "Stack Overflow",
+                href: "https://stackoverflow.com/questions/tagged/typegraphql",
+              },
+              {
+                label: "Twitter",
+                href: "https://twitter.com/typegraphql",
+              },
+            ],
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "GitHub",
+                href: "https://github.com/MichalLytek/typegraphql-prisma",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Michał Lytek`,
+      },
+      prism: {
+        theme: vsLightTheme,
+        darkTheme: vsDarkTheme,
+      },
+      algolia: {
+        appId: "G4ZB1BCPHE",
+        apiKey: "02a8d497c435602dcd39b67e73d7e53a",
+        indexName: "typegraphql_prisma",
+        // Optional: see doc section below
+        contextualSearch: true,
+        // Optional: Algolia search parameters
+        searchParameters: {
+          // facetFilters: ["type:content"],
+        },
+        // Set debug to true if you want to inspect the modal
+        debug: false,
+      },
+    }),
 };
+
+module.exports = config;
